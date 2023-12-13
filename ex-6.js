82d107a4b08dd02e368ddc6cc0d6d210cad02980
ณัฐพร นาคพงษ์ <Missterdonat33@gmail.com>
2023-12-13 14:04:12 +0700

    ex:es6 update

--- ex-6.js
+++ ex-6.js
@@ -1,4 +1,4 @@
 let height = undefined;
-let result = height ?? "Height is not defined";
+let result = (check) => check ?? "Height is not defined";
 
-console.log(result); //  Result ควรจะได้ออกมาเป็น “Height is not defined”
+console.log(result(height)); //  Result ควรจะได้ออกมาเป็น “Height is not defined”
